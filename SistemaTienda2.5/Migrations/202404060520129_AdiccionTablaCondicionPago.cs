290b9e9bee9e78c8867b066eae2dad6e2dec124c
hugoa <hugoa@HUGO-AQUINO>
2024-04-05 23:21:20 -0600

    MigracionTablaCondicionPago

---
+++ SistemaTienda2.5/Migrations/202404060520129_AdiccionTablaCondicionPago.cs
@@ -0,0 +1,30 @@
+﻿namespace SistemaTienda2._5.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AdiccionTablaCondicionPago : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.CondicionPago",
+                c => new
+                    {
+                        CondicionPagoId = c.Int(nullable: false, identity: true),
+                        Codigo = c.String(maxLength: 50),
+                        Descripcion = c.String(maxLength: 50),
+                        Estado = c.Boolean(nullable: false),
+                        Dias = c.Int(nullable: false),
+                        FechaCreacion = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.CondicionPagoId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.CondicionPago");
+        }
+    }
+}
