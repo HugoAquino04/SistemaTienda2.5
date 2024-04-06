fb9d496a8f98af3ce1364cc37cd170bef0fd24d3
hugoa <hugoa@HUGO-AQUINO>
2024-04-05 23:24:16 -0600

    MigracionTablaPedido

---
+++ SistemaTienda2.5/Migrations/202404060523280_AdiccionTablaPedido.cs
@@ -0,0 +1,32 @@
+﻿namespace SistemaTienda2._5.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AdiccionTablaPedido : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Pedido",
+                c => new
+                    {
+                        PedidoId = c.Int(nullable: false, identity: true),
+                        ClienteId = c.Int(nullable: false),
+                        FechaCreacion = c.DateTime(nullable: false),
+                        FechaPedido = c.DateTime(nullable: false),
+                        Estado = c.Boolean(nullable: false),
+                        Total = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        SubTotal = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Descuento = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.PedidoId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Pedido");
+        }
+    }
+}
