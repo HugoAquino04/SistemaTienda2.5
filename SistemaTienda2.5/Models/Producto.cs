565e421eb1af9cccac7d7fd6f917b5d51d5eacba
Milthon Mejia <milthonmejia30@gmail.com>
2024-04-05 23:31:02 -0600

    Agregue Tabla de Producto

---
+++ SistemaTienda2.5/Models/Producto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SistemaTienda2._5.Models
+{
+    [Table("Producto")]
+    public class Producto
+    {
+        [Key]
+        public int ProductoId { get; set; }
+
+        public int CategoriaId { get; set; }
+
+        public int UnidadMedidaId { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+        public bool Estado { get; set; }
+
+        public decimal PrecioCompra { get; set; } = 0.10m;
+    }
+}
