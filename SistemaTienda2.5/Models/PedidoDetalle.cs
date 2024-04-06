dae0c85f571cfa5b072d7f414a9ff911bfbcbd5c
Milthon Mejia <milthonmejia30@gmail.com>
2024-04-05 23:27:49 -0600

    Agregue Tabla de PedidoDetalle

---
+++ SistemaTienda2.5/Models/PedidoDetalle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SistemaTienda2._5.Models
+{
+    [Table("PedidoDetalle")]
+    public class PedidoDetalle
+    {
+        public int PedidoDetalleId { get; set; }
+
+        public int PedidoId { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+        public int ProductoId { get; set; }
+
+
+        public decimal Precio { get; set; } = 0.10m;
+
+
+        public decimal SubTotal { get; set; } = 0.10m;
+
+
+        public decimal Descuento { get; set; } = 0.10m;
+
+    }
+}
