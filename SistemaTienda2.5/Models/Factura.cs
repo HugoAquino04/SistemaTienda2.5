65b700bb4e150974a325fb835f47fb8d133f81a3
Alejandro Molina <molina34mmg@gmail.com>
2024-04-05 23:38:46 -0600

    Agregue tabal factura

---
+++ SistemaTienda2.5/Models/Factura.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaTienda2._5.Models
+{
+    [Table("Factura")]
+    public class Factura
+    {
+        [Key]
+        public int FacturaId { get; set; }
+
+        public int ClienteId { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+        public DateTime FechaFactura { get; set; }
+
+        public bool Estado { get; set; }
+
+        public decimal Total { get; set; } = 0.10m;
+
+        public decimal SubTotal { get; set; } = 0.10m;
+
+        public decimal Descuento { get; set; } = 0.10m;
+    }
+}
