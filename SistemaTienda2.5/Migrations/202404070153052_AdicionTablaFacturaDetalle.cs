ded42a067f37ccbbab3d524e8dad3cd5388d2417
hugoa <hugoa@HUGO-AQUINO>
2024-04-08 20:53:51 -0600

    Rutas

---
+++ SistemaTienda2.5/Migrations/202404070153052_AdicionTablaFacturaDetalle.cs
@@ -0,0 +1,16 @@
+﻿namespace SistemaTienda2._5.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AdicionTablaFacturaDetalle : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- SistemaTienda2.5/Models/FacturaDetalle.cs
+++ SistemaTienda2.5/Models/FacturaDetalle.cs
@@ -1,31 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Web;
 
 namespace SistemaTienda2._5.Models
 {
-    [Table("FacturaDetalle")]
     public class FacturaDetalle
     {
         [Key]
         public int FacturaDetalleId { get; set; }
-
         public int FacturaId { get; set; }
-
         public DateTime FechaCreacion { get; set; }
-
         public int ProductoId { get; set; }
-
         public decimal Precio { get; set; } = 0.10m;
-
         public decimal Total { get; set; } = 0.10m;
-
-        public decimal SubTotal { get; set; } = 0.10m;
-
+        public decimal Subtotal { get; set; } = 0.10m;
         public decimal Descuento { get; set; } = 0.10m;
-
     }
 }
